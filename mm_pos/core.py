d089b420c2c84123d3a01fe7da0f1f734ccc771f
Kelvin Lin <kelvin@bu.edu>
2025-08-22 14:52:50 -0400

    Add initial files for pypi

---
+++ mm_pos/core.py
@@ -0,0 +1,4 @@
+# mm_pos/core.py
+
+def hello():
+    return "Welcome to mm-pos! Your open-source POS system is ready."
---
+++ tests/test_core.py
@@ -0,0 +1,4 @@
+from mm_pos import hello
+
+def test_hello():
+    assert hello() == "Welcome to mm-pos! Your open-source POS system is ready."
